36f4a5bd0cc36624097c9ff79a8d3bde762636e2
Aryan <aryanrworks@gmail.com>
2025-12-06 04:09:08 +0530

    Order.h

--- Order.h
+++ Order.h
@@ -8,17 +8,15 @@ enum class Side { Buy, Sell };
 enum class OrderType { Market, Limit };
 
 struct Order {
-    int id;             // Unique Order ID
-    double price;       // Price per share
-    int quantity;       // Number of shares
-    Side side;          // Buy or Sell
-    OrderType type;     // Market or Limit
+    int id;            
+    double price;       
+    int quantity;     
+    Side side;        
+    OrderType type;    
 
-    // Constructor
     Order(int _id, double _price, int _qty, Side _side, OrderType _type)
         : id(_id), price(_price), quantity(_qty), side(_side), type(_type) {}
 };
 
-// Typedefs for cleaner code later
 using OrderPtr = std::shared_ptr<Order>;
 using OrderList = std::list<OrderPtr>;
