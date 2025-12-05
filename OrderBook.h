c737ed68f30d3b2ad707057a1a214d706587527f
Aryan <aryanrworks@gmail.com>
2025-12-06 04:10:08 +0530

    OrderBook.h

--- OrderBook.h
+++ OrderBook.h
@@ -6,19 +6,14 @@
 
 class OrderBook {
 private:
-    // Bids: Sorted High -> Low
     std::map<double, OrderList, std::greater<double>> bids;
     
-    // Asks: Sorted Low -> High
     std::map<double, OrderList, std::less<double>> asks;
 
-    // Helper functions to execute trades
     void matchOrders();
 
 public:
-    // Main interface
     void addOrder(int id, double price, int qty, Side side, OrderType type);
     
-    // For visualization/debugging
     void printBook();
 };
